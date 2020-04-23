d9741da6aabbf3042963da7341535e38fe20e832
Володимир <onmaine@gmail.com>
2020-04-23 14:51:13 +0300

    1st task

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,1 +1,19 @@
-// Your code goes here
+let billAmount = parseFloat(prompt('What is your total bill?'));
+let tipPersent = parseFloat(prompt('How much do you want to tip'));
+let tip = tipPersent / 100;
+let tipTotal = tip * billAmount;
+let finalCost = billAmount + tipTotal;
+
+
+if (isNaN(billAmount) || isNaN(tipPersent)) {
+  alert('Invalid input data');
+} else if (billAmount < 0 || tipPersent < 0 || tipPersent >100){
+  alert('Invalid input data');
+} else {
+  alert(`Check number: ${billAmount} \nTip: ${tipPersent}% \nTip amount: ${tipTotal.toFixed(2)} \nTotal sum to pay: ${finalCost.toFixed(2)}`);
+
+    console.log('The billAmount is ' + billAmount);
+    console.log('The tipAmount is ' + tipPersent);
+    console.log('The tip is ' + tipTotal);
+    console.log('The total sum to pay is ' + finalCost);
+}
--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,1 +1,24 @@
-// Your code goes here
+let billAmount = parseFloat(prompt('What is your total bill?'));
+let tipAmount = parseFloat(prompt('How much do you want to tip'));
+
+if(isNaN(billAmount) || isNaN(tipAmount)){
+     alert('Invalid input data');
+} else if (billAmount < 0 || tipAmount < 0){
+         alert('Invalid input data');
+    } else {
+        alert(
+          `Check number: ${billAmount} \nTip: ${tipAmount} \nTip amount: ${tip} \nTotal sum to pay: ${tipTotal}`
+        );
+
+
+
+    let tip = tipAmount / 100;
+    let tipTotal = tip * billAmount;
+
+    console.log('The tip is ' + tipTotal);
+    console.log('The total bill is ' + finalCost(billAmount, tipTotal));
+}
+
+function finalCost(billAmount, tipTotal) {
+  return billAmount + tipTotal;
+}
