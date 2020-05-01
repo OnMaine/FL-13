50c89b0175f39438ed0d4926148d6405909650d5
Володимир <onmaine@gmail.com>
2020-05-01 22:30:14 +0300

    6 task

---
+++ FL13_HW8/homework/js/letterCount.js
@@ -0,0 +1,14 @@
+function letterCount(str, letter) {
+  let totalCount = 0;
+  for (let position = 0; position < str.length; position++){
+     let strLowerCase = str.toLowerCase();
+     if (strLowerCase.charAt(position) == letter){
+     totalCount += 1;
+     }
+  }
+  return totalCount;
+}
+
+console.log(letterCount("Maggy", "g"));
+console.log(letterCount("Barry", "b"));
+console.log(letterCount("", "z"));
