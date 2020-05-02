71f0eb91a27a65c66dd0188487b0c0769a3ed132
Володимир <onmaine@gmail.com>
2020-05-02 23:16:28 +0300

    added console.log

--- FL13_HW8/homework/js/countPoints.js
+++ FL13_HW8/homework/js/countPoints.js
@@ -9,4 +9,5 @@ function countPoints(gameResults){
   }, 0);
 }
 
+console.log(countPoints(['3:1', '1:0', '0:0', '1:2', '4:0', '2:3', '1:1', '0:1', '2:1', '1:0']));
 console.log(countPoints(['1:1', '1:2', '2:0', '4:2', '0:1', '2:3', '1:1', '0:1', '1:1', '3:0']));
