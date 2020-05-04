d158b2d1a38d93ba421b8842c0abc7391d48f004
Володимир <onmaine@gmail.com>
2020-05-05 01:16:08 +0300

    HW 9 tasks 2 3 4 6 7

--- FL13_HW9/homework/index.js
+++ FL13_HW9/homework/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+function makeListFromRange(arr) {
+    let [start, stop] = arr;
+    let list = [start], b = start;
+    let step = 1;
+    while (b < stop) {
+        list.push(b += step);
+    }
+    return list;
+}
+
+
+console.log(makeListFromRange([3, 7]));
