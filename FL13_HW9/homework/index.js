270662e9498973a33e2f15c8182120972ac761fb
Володимир <onmaine@gmail.com>
2020-05-05 03:32:16 +0300

    HW 9 eslint 1-7 tasks

--- FL13_HW9/homework/index.js
+++ FL13_HW9/homework/index.js
@@ -1,7 +1,7 @@
-function convert(stringToSplit) {
-    result = [];
+function convert() {
+    let result = [];
     for (let i = 0; i < arguments.length; i++) {
-        if (typeof(arguments[i]) === "string") {
+        if (typeof arguments[i] === 'string') {
             result.push(Number(arguments[i]));
         } else {
           result.push(String(arguments[i]));
@@ -9,7 +9,54 @@ function convert(stringToSplit) {
     }
     return result;
 }
+convert();
 
-console.log(convert('1', 2, 3, '4'));
+function executeforEach(arr, func) {
+    for (let i = 0; i < arr.length; i++) {
+        func(arr[i]);
+    }
+    return arr;
+}
 
-console.log(typeof result[1]);
+executeforEach();
+
+function mapArray(arr, func) {
+    let array =[];
+    executeforEach(arr,function(el) {
+        array.push(func(el));
+    });
+    return array;
+}
+mapArray();
+
+function filterArray(arr, func) {
+    let filteredArr = [];
+    executeforEach(arr, function(el) {
+        if (func(el)) {
+            filteredArr.push(el);
+        }
+    });
+    return filteredArr;
+}
+filterArray();
+//5
+
+function flipOver(str) {
+    let newStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str.charAt(i);
+    }
+    return newStr;
+}
+flipOver();
+//7
+function makeListFromRange(arr) {
+    let [start, stop] = arr;
+    let list = [start], b = start;
+    let step = 1;
+    while (b < stop) {
+        list.push(b += step);
+    }
+    return list;
+}
+makeListFromRange();
