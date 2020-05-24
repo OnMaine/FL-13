2f9e77e280fbce51b941933d8780e52e50efe052
Володимир <onmaine@gmail.com>
2020-05-24 15:42:43 +0300

    hw15 task1

--- FL13_HW15/homework/js/task1.js
+++ FL13_HW15/homework/js/task1.js
@@ -1,1 +1,16 @@
-// TODO: Your code goes here
+let assign = function(obj, creditCard, paymentCard) {
+    this.obj = obj;
+    this.creditCard = creditCard;
+    this.paymentCard = paymentCard;
+    let i = 0;
+    let il = arguments.length;
+    let key;
+    for (; i < il; i++) {
+        for (key in arguments[i]) {
+            if (arguments[i].hasOwnProperty(key)) {
+                obj[key] = arguments[i][key];
+            }
+        }
+    }
+    return console.log(obj);
+};
