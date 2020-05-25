99625a68c13ec961783df9952d4209017b942b26
Володимир <onmaine@gmail.com>
2020-05-25 21:59:12 +0300

    HW 15 90% complited

--- FL13_HW15/homework/js/task2.js
+++ FL13_HW15/homework/js/task2.js
@@ -1,34 +1,83 @@
+function inheritance(child, parent) {
+  child.prototype = Object.create(parent.prototype);
+  child.prototype.constructor = child;
+}
+
 function Vehicle(engine, color) {
   this.engine = engine;
   this.color = color;
-  let maxSpeed = 70;
-  let model = 'unknown model';
-  this.getInfo = function() {
-    return {
-   engine,
-   color,
-   maxSpeed,
-   model
- };
+  this.model = 'unknown model';
+  this.maxSpeed = 70;
+  this.currentSpeed = 0;
+  this.maxSpeedFromDrive = 0;
+}
+
+Vehicle.prototype.getInfo = function() {
+  let info = {}
+  info.engine = this.engine;
+  info.color = this.color;
+  info.model = this.model;
+  info.maxSpeed = this.maxSpeed;
+  return info;
+};
+
+Vehicle.prototype.drive = function() {
+  clearInterval(this.timerId);
+  let startInterval = 2000;
+  this.timerId = setInterval(() => {
+    this.currentSpeed += 20
+    console.log(this.currentSpeed)
+    if (this.currentSpeed > this.maxSpeed) {
+      console.log('speed is too high, SLOW DOWN!');
+    }
+    if (this.currentSpeed > this.maxSpeedFromDrive) {
+      this.maxSpeedFromDrive = this.currentSpeed
+    }
+  }, startInterval)
+};
+
+Vehicle.prototype.stop = function() {
+  clearInterval(this.timerId);
+  let stopInterval = 1500;
+  this.timerId = setInterval(() => {
+    if (this.currentSpeed > 0) {
+      this.currentSpeed -= 20
+      console.log(this.currentSpeed)
+      if (this.currentSpeed === 0) {
+        clearInterval(this.timerId);
+        console.log(`Vehicle is stopped. Maximum speed during the drive was ${this.maxSpeedFromDrive}`);
+      }
+    } else {
+      clearInterval(this.timerId);
+    }
+  }, stopInterval)
+};
+
+Vehicle.prototype.upgradeEngine = function(newEngine, maxSpeedUpGrade) {
+  if (this.currentSpeed === 0) {
+    this.engine = newEngine;
+    this.maxSpeed = maxSpeedUpGrade;
+  }
+};
+
+inheritance(Car, Vehicle);
+inheritance(Motorcycle, Vehicle);
+
+function Car(engine, color, model) {
+  Vehicle.call(this, engine, color);
+  this.maxSpeed = 80;
+  this.model = model;
+  this.changeColor = function(newColor) {
+    if (!(this.color === newColor)) {
+      this.color = newColor;
+    } else {
+      console.log(`The car is already painted ${this.color}`);
+    }
   }
 }
 
-function Car(engine, color, model) {
+function Motorcycle(engine, color, model) {
+  Vehicle.call(this, engine, color);
+  this.maxSpeed = 90;
   this.model = model;
-  this.engine = engine;
-  this.color = color;
 }
-
-Car.prototype = Vehicle;
-
-// functionion Car() {
-//   Vehicle.call(this);
-//   this.model = model;
-//   let maxSpeed = 80;
-// };
-//
-// function Motorcycle() {
-//   Vehicle.call(this);
-//   this.model = model;
-//   let maxSpeed = 90;
-// };
