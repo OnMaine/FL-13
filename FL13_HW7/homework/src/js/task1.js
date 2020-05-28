8f44cd18809929dea8dbe7d3faf69741ad06808f
Володимир <onmaine@gmail.com>
2020-05-29 01:31:11 +0300

    HW 7 JS 1. user verification 2. roulette gambling

--- FL13_HW7/homework/src/js/task1.js
+++ FL13_HW7/homework/src/js/task1.js
@@ -1,4 +1,4 @@
-let login = prompt('Login','');
+let login = prompt('Login', '');
 let password = '';
 let emptyLogin = login === null || login.length === 0;
 const loginLength = 4;
@@ -8,27 +8,33 @@ let dayHours = new Date('2020-04-26T08:00:00').getHours();
 let nightHours = new Date('2020-04-26T20:00:00').getHours();
 const timeRange = currentHours >= dayHours && currentHours < nightHours;
 
-let user = { login:'User', password: 'UserPass' };
-let admin = { login:'Admin', password: 'RootPass' };
+let user = {
+  login: 'User',
+  password: 'UserPass'
+};
+let admin = {
+  login: 'Admin',
+  password: 'RootPass'
+};
 
 if (login === user.login || login === admin.login) {
-    password = prompt('Enter your password');
-    if (login === user.login && password === user.password ||
+  password = prompt('Enter your password');
+  if (login === user.login && password === user.password ||
     login === admin.login && password === admin.password) {
-      if (timeRange) {
-        alert('Good day, dear ' + login + '!');
-      } else {
-        alert('Good evening, dear '+ login + '!');
-      }
-    } else if (password === null) {
-        alert('Canceled.');
+    if (timeRange) {
+      alert('Good day, dear ' + login + '!');
     } else {
-        alert('Wrong password');
+      alert('Good evening, dear ' + login + '!');
     }
-} else if (emptyLogin) {
+  } else if (password === null) {
     alert('Canceled.');
+  } else {
+    alert('Wrong password');
+  }
+} else if (emptyLogin) {
+  alert('Canceled.');
 } else if (shortlogin) {
-    alert('I don’t know any users having name length less than 4 symbols');
+  alert('I don’t know any users having name length less than 4 symbols');
 } else {
-    alert('I don’t know you');
+  alert('I don’t know you');
 }
