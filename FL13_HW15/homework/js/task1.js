7c8b087f3803808b77dfeb292ce90547e5e53a27
Володимир <onmaine@gmail.com>
2020-05-29 01:40:41 +0300

    HW 15 js Inheritance

--- FL13_HW15/homework/js/task1.js
+++ FL13_HW15/homework/js/task1.js
@@ -1,16 +1,16 @@
 let assign = function(obj, creditCard, paymentCard) {
-    this.obj = obj;
-    this.creditCard = creditCard;
-    this.paymentCard = paymentCard;
-    let i = 0;
-    let il = arguments.length;
-    let key;
-    for (; i < il; i++) {
-        for (key in arguments[i]) {
-            if (arguments[i].hasOwnProperty(key)) {
-                obj[key] = arguments[i][key];
-            }
-        }
+  this.obj = obj;
+  this.creditCard = creditCard;
+  this.paymentCard = paymentCard;
+  let i = 0;
+  let il = arguments.length;
+  let key;
+  for (; i < il; i++) {
+    for (key in arguments[i]) {
+      if (arguments[i].hasOwnProperty(key)) {
+        obj[key] = arguments[i][key];
+      }
     }
-    return console.log(obj);
+  }
+  return console.log(obj);
 };
